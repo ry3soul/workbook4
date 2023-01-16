4e56107e66c56f57abe6fd7ee499bbb085d40484
ry3soul <121807404+ry3soul@users.noreply.github.com>
2023-01-16 11:22:41 +0000

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,18 @@
+//sql and bootstrap table 
+// add information to table from sql
+
+
+if (mysqli_num_rows($result) > 0) {    
+        while($row = mysqli_fetch_array($result)) {
+            echo "<tr> ";
+            echo " <td>" . $row["id"] . "</td>";
+            echo " <td>" . $row["name"] . "</td>";
+            echo " <td>" . $row["region"] . "</td>";
+            echo " <td>" . $row["value"] . "</td>";
+            echo "</tr>\n";       // Add a newline at the end
+        }
+    } else {
+        echo "0 results";
+    }
+			mysqli_close($conn);	// remember to close db connection
 
