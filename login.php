f4d565345c52ec1b4b6b99990e75dedc1b54ec44
Rojeen <121807404+ry3soul@users.noreply.github.com>
2023-01-16 11:39:46 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,80 @@
+//improved login page can redirect to two different pages 
 
+
+<?php
+
+
+
+$VAILD_EMAIL = "member@gmail.com";
+
+$VAILD_PASSWORD = "member123";
+
+$ADMIN_EMAIL = "admin@gmail.com";
+
+$ADMIN_PASSWORD = "admin123";
+
+
+
+// Check if the form has been submitted
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  // Sanitize user input
+
+  $credentials = [
+
+    'email' => filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL),
+
+    'password' => filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING)
+
+  ];
+
+
+
+  // Check if the email and password match a valid user
+
+  if ($VAILD_EMAIL == $credentials['email'] && $VAILD_PASSWORD == $credentials['password']) {
+
+    redirect('home.html');
+
+  } 
+
+  // Check if the email and password match the admin user
+
+  else if ($ADMIN_EMAIL == $credentials['email'] && $ADMIN_PASSWORD == $credentials['password']) {
+
+    redirect('teacherhome.html');
+
+  }
+
+  // If the email and password don't match any valid user, redirect to the login page
+
+  else {
+
+    $_SESSION["status"] = "NotLoggedIn";
+
+    $_SESSION['login_error_msg'] = "Sorry, that user name or password is incorrect. Please try again.";
+
+    header("Location: home.html");
+
+    exit();
+
+  }
+
+}
+
+
+
+// Redirect to the given URL
+
+function redirect($url) {
+
+  header("Location: $url");
+
+  exit();
+
+}
+
+
+
+?>
